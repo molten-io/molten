3210154ab6777a524a14608aed9d2400dd666ccd
molten-io <mchayat+moltenioatgithub@gmail.com>
2016-07-22 22:29:29 -0700

    Added creds vars

--- molten/test-mule/terraform/main.tf
+++ molten/test-mule/terraform/main.tf
@@ -2,12 +2,17 @@ variable "file" {
 	default = "../target/test-mule-0.0.1-SNAPSHOT.jar"
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 variable "region" {
-	default = "ap-southeast-2"
+	default = "us-east-1"
 }
 
 provider "aws" {
 	region = "${var.region}"
+	access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -34,7 +39,8 @@ resource "aws_lambda_function" "test_lambda" {
     function_name = "HelloMule"
     role = "${aws_iam_role.iam_for_lambda.arn}"
     handler = "LambdaFunctionHandler.handleRequest"
-	runtime = "Java8"
+	runtime = "java8"
+	timeout = 120
 }
 
 resource "aws_api_gateway_rest_api" "test_api" {
